cfadb19f1bc918a8349dc2de88edb4a629f7517c
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-01-04 13:48:59 +0100

    feat: add more logging to db connection

--- app/db/connectDb.server.js
+++ app/db/connectDb.server.js
@@ -22,12 +22,15 @@ export default function connectDb() {
   // Reuse the existing Mongoose connection if we have one...
   // https://mongoosejs.com/docs/api/connection.html#connection_Connection-readyState
   if (mongoose.connection.readyState > 0) {
+    console.log("Re-using existing Mongoose connection (readyState=%d)", mongoose.connection.readyState);
     // ...but overwrite all models in development to ensure we pick up any changes made in schemas
     if (NODE_ENV === "development") {
       for (const model of models) {
         if (mongoose.connection.models[model.name]) {
+          console.log("Deleting existing Mongoose model: %s", model.name);
           mongoose.connection.deleteModel(model.name);
         }
+        console.log("Creating Mongoose model: %s", model.name);
         mongoose.connection.model(model.name, model.schema, model.collection);
       }
     }
@@ -36,21 +39,22 @@ export default function connectDb() {
   }
 
   // If no connection exists yet, set up event logging...
-  mongoose.connection.on("connected", () => {
-    console.log("Mongoose connected, NODE_ENV=%s", NODE_ENV);
-  });
-
-  mongoose.connection.on("disconnected", () => {
-    console.log("Mongoose DISCONNECTED, NODE_ENV=%s", NODE_ENV);
-  });
+  mongoose.connection.on('connected', () => console.log('Mongoose: connected, NODE_ENV=%s', NODE_ENV));
+  mongoose.connection.on('disconnected', () => console.log('Mongoose: disconnected'));
+  mongoose.connection.on('reconnected', () => console.log('Mongoose: reconnected'));
+  mongoose.connection.on('disconnecting', () => console.log('Mongoose: disconnecting'));
+  mongoose.connection.on('close', () => console.log('Mongoose: close'));
 
   // ...and create a new connection:
-  mongoose.connect(MONGODB_URL);
+  mongoose.connect(MONGODB_URL).catch((error) => {
+    console.error(error);
+  });
 
   // "Models are always scoped to a single connection."
   // https://mongoosejs.com/docs/connections.html#multiple_connections
   // So we set them up here to avoid overwriting and getting errors in dev mode.
   for (const model of models) {
+    console.log("Creating Mongoose model: %s", model.name);
     mongoose.connection.model(model.name, model.schema, model.collection);
   }
 
--- app/db/models.js
+++ app/db/models.js
@@ -10,9 +10,14 @@ const bookSchema = new Schema(
       minLength: [3, "That's too short"],
     },
   },
+  // Automatically add `createdAt` and `updatedAt` timestamps:
+  // https://mongoosejs.com/docs/timestamps.html
   { timestamps: true },
 );
 
+// For each model you want to create, please define the model's name, the
+// associated schema (defined above), and the name of the associated collection
+// in the database (which will be created automatically).
 export const models = [
   {
     name: "Book",
